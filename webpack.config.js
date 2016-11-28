c0a18b4b06349311e864c4f1399184607368f27f
DrewWarrenTIY <drewwarrentiy@gmail.com>
2016-11-28 13:42:23 -0700

    core(chore) Add babel and other packages to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,10 +6,28 @@ module.exports = {
   },
   module: {
     loaders: [
-        {
-          test: /\.css$/,
-          loader: "style!css"
+      {
+        test: /\.js?/,
+        include: SRC_DIR,
+        loader: "babel-loader",
+        query: {
+          presets: ["react", "es2015"]
         }
-      ]
+      },
+      {
+       test: /\.json?/,
+       include: SRC_DIR,
+       loader: "json-loader"
+      },
+      {
+        test: /\.css$/,
+        loader: "style!css"
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)(\?v=\d+\.\d+\.\d+)?$/i,
+        loader: 'file-loader?name=[path][name].[ext]?[hash:10]',
+         exclude: /(node_modules|bower_components)/
+      }
+    ]
     }
   };
