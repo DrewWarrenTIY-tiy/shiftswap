d585776bbdfa22eebd5e37ad26fc66e18c00b2dd
DrewWarrenTIY <drewwarrentiy@gmail.com>
2016-11-28 13:36:43 -0700

    feat(init) Initialize Webpack, link JS and CSS to HTML

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    entry: "./src/scripts/entry.js",
+    output: {
+      path: __dirname,
+      filename: "./public/bundle.js"
+  },
+  module: {
+    loaders: [
+        {
+          test: /\.css$/,
+          loader: "style!css"
+        }
+      ]
+    }
+  };
