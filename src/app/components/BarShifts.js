1e6f9a57d1c5e348fa004a53e493344201686964
DrewWarrenTIY <drewwarrentiy@gmail.com>
2016-11-30 15:45:34 -0700

    WIP Dev bind dropdown to Firebase

--- src/app/components/BarShifts.js
+++ src/app/components/BarShifts.js
@@ -14,7 +14,7 @@ export default class BarShifts extends React.Component {
 
     return (
       <div className="barShifts">
-         <p>Mon AM: {this.props.barShifts[0]} <EmpList emplData={this.props.emplData} /></p>
+         <div className="">Mon AM: {this.props.barShifts[0]} <EmpList emplData={this.props.emplData} /></div>
          <p>Tues AM: {this.props.barShifts[1]}</p>
          <p>Wed AM: {this.props.barShifts[2]}</p>
          <p>Thurs AM: {this.props.barShifts[3]}</p>
--- src/app/components/EmpList.js
+++ src/app/components/EmpList.js
@@ -4,24 +4,49 @@ import React from 'react';
 
 var firebase = require("firebase");
 
+
 export default class EmpList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: 'open'};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    console.log("dropdown value: " + this.state.value);
+    event.preventDefault();
+  }
 
   render () {
+
+    function setEmpl(str) {
+      console.log(str);
+    }
 
     let emplData = this.props.emplData;
 
     return (
       <div className='empList'>
-        <select className='dropDown'>
-          <option  value="open">open</option>
-          {this.props.emplData.map(function(c,i,a) {
-            return (
-                <option key={"Employee" + i} value={a[i]}>
-                {a[i]}
-              </option>
-            )
-          })}
-        </select>
+        <form onSubmit={this.handleSubmit}>
+          <select className='dropDown' value={this.state.value} onChange={this.handleChange}>
+            <option  value="open">open</option>
+            {this.props.emplData.map(function(c,i,a) {
+              return (
+                  <option key={"Employee" + i} value={a[i]}>
+                  {a[i]}
+                </option>
+              )
+            })}
+            <option value="none">none</option>
+          </select>
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     )
   }
