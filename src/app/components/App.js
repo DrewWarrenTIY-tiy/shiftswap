06930095c3d7a62b8ae1728aa394ee1e19b75111
DrewWarrenTIY <drewwarrentiy@gmail.com>
2016-12-06 12:12:45 -0700

    WIP Successfully pull FBDB to App

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -9,17 +9,91 @@ import Manager from './Manager';
 
 import container from './App.css';
 
+var firebase = require("firebase");
+
+// Set the configuration for your app
+var config = {
+  apiKey: "AIzaSyDayn7zcog-05rzJNH6KeQyHTyttuzUT_8",
+  authDomain: "shiftswapforsues.firebaseapp.com",
+  databaseURL: "https://shiftswapforsues.firebaseio.com",
+  storageBucket: "shiftswapforsues.appspot.com"
+};
+firebase.initializeApp(config);
+
+// Get a reference to the database service
+var fbdbRef = firebase.database().ref();
+var fbdbEmpl = fbdbRef.child('employees');
+var fbdbBarShifts = fbdbRef.child('barshifts');
+
+let barShifts = [];
+let barShiftsKeys = [];
+let emplData = [];
+
+function displayEmplData(val, key) {
+  emplData.push(val)
+}
+
 export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      barShifts: barShifts,
+      barShiftsKeys: barShiftsKeys,
+      emplData: emplData,
+      fbdbRef: fbdbRef
+    }
+  }
+
+  componentWillMount () {
+    fbdbEmpl.on("child_added", (snapshot) => {
+      displayEmplData(snapshot.val(), snapshot.key);
+      this.setState({
+        emplData: emplData
+      });
+    }).bind(this)
+    const fbdbBarShiftsRef = this.state.fbdbRef.child('barshifts');
+    //POPULATES OBJECT KEYS ARRAY
+    fbdbBarShiftsRef.on('value', snapshot => {
+      let barShiftsObj = snapshot.val();
+      let barShiftsKeys = Object.keys(barShiftsObj);
+      for (let i = 0; i < barShiftsKeys.length; i++) {
+        barShiftsKeys[i] = barShiftsKeys[i];
+      }
+      this.setState({
+        barShiftsKeys: barShiftsKeys
+      });
+    });
+    //POPULATES OBJECT VALS ARRAY
+    fbdbBarShiftsRef.on('value', snapshot => {
+      let barShiftsObj = snapshot.val();
+      let barShifts = Object.values(barShiftsObj);
+      for (let i = 0; i < barShifts.length; i++) {
+        barShifts[i] = barShifts[i];
+      }
+      this.setState({
+        barShifts: barShifts
+      });
+    });
+  };
 
   render () {
     return (
       <Router>
-        <div className="container">
+        <div className='container'>
           <Header />
-          <Match exactly pattern ='/' component={Manager}/>
+          <Match exactly pattern="/" render={
+            (defaultProps) => <Manager barShifts={this.state.barShifts}
+            barShiftsKeys={this.state.barShiftsKeys}
+            emplData={this.state.emplData}
+            fbdbRef={this.state.fbdbRef} {...defaultProps} />
+          }/>
           <Match pattern='/bar' component={Bar}/>
         </div>
       </Router>
-    );
+    )
   }
 }
+
+
+
+      // <Match exactly pattern ='/' component={Manager}/>
--- src/app/components/Manager.js
+++ src/app/components/Manager.js
@@ -9,78 +9,21 @@ import manager from './Manager.css';
 
 var firebase = require("firebase");
 
-// Set the configuration for your app
-var config = {
-  apiKey: "AIzaSyDayn7zcog-05rzJNH6KeQyHTyttuzUT_8",
-  authDomain: "shiftswapforsues.firebaseapp.com",
-  databaseURL: "https://shiftswapforsues.firebaseio.com",
-  storageBucket: "shiftswapforsues.appspot.com"
-};
-firebase.initializeApp(config);
-
-// Get a reference to the database service
-var fbdbRef = firebase.database().ref();
-var fbdbEmpl = fbdbRef.child('employees');
-var fbdbBarShifts = fbdbRef.child('barshifts');
-
-let barShifts = [];
-let barShiftsKeys = [];
-let emplData = [];
-
-function displayEmplData(val, key) {
-  emplData.push(val)
-}
-
 export default class Manager extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      barShifts: barShifts,
-      barShiftsKeys: barShiftsKeys,
-      emplData: emplData,
-      fbdbRef: fbdbRef
-    }
-  }
-
-  componentWillMount () {
-    fbdbEmpl.on("child_added", (snapshot) => {
-      displayEmplData(snapshot.val(), snapshot.key);
-      this.setState({
-        emplData: emplData
-      });
-    }).bind(this)
-    const fbdbBarShiftsRef = this.state.fbdbRef.child('barshifts');
-    //POPULATES OBJECT KEYS ARRAY
-    fbdbBarShiftsRef.on('value', snapshot => {
-      let barShiftsObj = snapshot.val();
-      let barShiftsKeys = Object.keys(barShiftsObj);
-      for (let i = 0; i < barShiftsKeys.length; i++) {
-        barShiftsKeys[i] = barShiftsKeys[i];
-      }
-      this.setState({
-        barShiftsKeys: barShiftsKeys
-      });
-    });
-    //POPULATES OBJECT VALS ARRAY
-    fbdbBarShiftsRef.on('value', snapshot => {
-      let barShiftsObj = snapshot.val();
-      let barShifts = Object.values(barShiftsObj);
-      for (let i = 0; i < barShifts.length; i++) {
-        barShifts[i] = barShifts[i];
-      }
-      this.setState({
-        barShifts: barShifts
-      });
-    });
-  };
 
   render () {
+
+    let barShifts = this.props.barShifts;
+    let barShiftsKeys = this.props.barShiftsKeys;
+    let emplData = this.props.emplData;
+    let fbdbRef = this.props.fbdbRef;
+
     return (
       <div className='manager'>
-        <BarShifts barShifts={this.state.barShifts}
-        barShiftsKeys={this.state.barShiftsKeys}
-        emplData={this.state.emplData}
-        fbdbRef={this.state.fbdbRef} />
+        <BarShifts barShifts={barShifts}
+        barShiftsKeys={barShiftsKeys}
+        emplData={emplData}
+        fbdbRef={fbdbRef} />
       </div>
     )
   }
